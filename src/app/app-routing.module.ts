3b2887e6376a32cf0cd36e7513cc3c18eb0508a1
DaniOrze <daniorzechowski@gmail.com>
2023-08-03 11:07:43 -0300

    feat: creating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserListPageComponent } from './components/user-list-page/user-list-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { NewUserPageComponent } from './components/new-user-page/new-user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: UserListPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'new-user', component: NewUserPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
